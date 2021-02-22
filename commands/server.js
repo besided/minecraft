9dd77209f441d383a1edbded96804c2c3e6ce079
tuezs <jontuezk@gmail.com>
2021-02-21 20:39:44 -0500

    fixed

--- commands/server.js
+++ commands/server.js
@@ -30,7 +30,6 @@ module.exports = {
                 {name: 'Modpack:', value: 'FTB: Revelation'}
             )
             .setFooter(`Requested by: ${member.username}`, (avatar))
- 
             message.channel.send(embed);
         })
 
